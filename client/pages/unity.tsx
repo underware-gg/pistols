5b43ffc4ec259887940e5ba4ede791ca4022dec8
R13 <rbentos@yahoo.com>
2023-12-19 19:17:01 -0300

    Unity build test 1 at /unity

---
+++ client/pages/unity.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Unity, useUnityContext } from 'react-unity-webgl'
+import App from '@/pistols/components/App'
+import { ActionButton } from '@/pistols/components/ui/Buttons';
+
+export default function UnityPage() {
+
+  const buildSuffix = '/unity/test1/Export'
+  const { unityProvider, sendMessage } = useUnityContext({
+    loaderUrl: `${buildSuffix}.loader.js`,
+    dataUrl: `${buildSuffix}.data`,
+    frameworkUrl: `${buildSuffix}.framework.js`,
+    codeUrl: `${buildSuffix}.wasm`,
+  });
+
+  return (
+    <App>
+      <Unity className='UnityCanvas' unityProvider={unityProvider} />
+      <SceneButton sendMessage={sendMessage} sceneName='Scene A' />
+      <SceneButton sendMessage={sendMessage} sceneName='Scene B' />
+      <SceneButton sendMessage={sendMessage} sceneName='Another Scene' />
+     </App>
+  )
+}
+
+function SceneButton({
+  sceneName,
+  sendMessage,
+}) {
+  const _select = () => {
+    sendMessage('GameController', 'SetScene', sceneName);
+  }
+  return <ActionButton label={sceneName} onClick={() => _select()} />
+}
+
