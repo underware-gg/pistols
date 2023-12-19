394e4aacb4f227abdd7c5254baa00e1dd3d4eea9
Andrew Morris <voltrevo@gmail.com>
2023-12-19 14:44:03 +1100

    Forge init

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: CC0
 pragma solidity ^0.8.13;
 
 contract Counter {
--- contracts/test/Counter.t.sol
+++ contracts/test/Counter.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: CC0
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
