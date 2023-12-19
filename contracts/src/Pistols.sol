07ee0ffe5e0bcbedbe2945c4fe3c2b2ea4b73f69
Andrew Morris <voltrevo@gmail.com>
2023-12-19 21:56:39 +1100

    Implement shootout

--- contracts/src/Pistols.sol
+++ contracts/src/Pistols.sol
@@ -11,15 +11,26 @@ struct PlayerGameData {
 
     bytes32 battleChoiceCommitment;
     uint256 battleChoice;
+
+    bool injured;
 }
 
 struct Game {
     uint256 state;
     uint256 stake;
     uint256 activityDeadline;
+    bytes32 rand;
 
     PlayerGameData player1;
     PlayerGameData player2;
+}
+
+struct PlayerStats {
+    bool init;
+    uint256 wins;
+    uint256 losses;
+    uint256 draws;
+    uint256 dishonors;
 }
 
 contract Pistols {
@@ -30,6 +41,7 @@ contract Pistols {
 
     uint256 public nextGameId = 0;
     mapping(uint256 => Game) public games;
+    mapping(address => PlayerStats) public allPlayerStats;
 
     event GameCreated(
         uint256 gameId,
@@ -38,6 +50,7 @@ contract Pistols {
     );
     event GameUpdated(uint256 gameId);
     event AssertionFailed(string message);
+    event Dishonor(uint256 gameId, address indexed player);
 
     uint256 constant public MIN_STAKE = 25_000_000_000_000_000_000;
     uint256 constant public FULL_STEP_COUNT = 10;
@@ -52,6 +65,10 @@ contract Pistols {
     uint256 constant STATE_BATTLE = 7;
 
     uint256 constant UNSET = type(uint256).max;
+
+    uint256 constant INJURED = 1 << 0;
+    uint256 constant KILLED = 1 << 1;
+    uint256 constant DISHONORED = 1 << 2;
 
     constructor(
         address _owner,
@@ -74,26 +91,29 @@ contract Pistols {
             state: STATE_CHALLENGE,
             activityDeadline: block.timestamp + timeout,
             stake: stake,
+            rand: 0,
             player1: PlayerGameData({
                 addr: msg.sender,
                 shootStepCommitment: bytes32(UNSET),
                 shootStep: UNSET,
                 battleChoiceCommitment: bytes32(UNSET),
-                battleChoice: UNSET
+                battleChoice: UNSET,
+                injured: false
             }),
             player2: PlayerGameData({
                 addr: challengee,
                 shootStepCommitment: bytes32(UNSET),
                 shootStep: UNSET,
                 battleChoiceCommitment: bytes32(UNSET),
-                battleChoice: UNSET
+                battleChoice: UNSET,
+                injured: false
             })
         });
 
         emit GameCreated(gameId, msg.sender, challengee);
     }
 
-    function cleanUpTimedOutGame(uint256 gameId) external {
+    function finishTimedOutGame(uint256 gameId) external {
         Game storage game = games[gameId];
         requireTimedOut(game);
 
@@ -131,20 +151,13 @@ contract Pistols {
             revert("Unexpected state");
         }
 
-        // TODO: Dishonor the player(s) who timed out
-
-        if (winner == 0) {
-            uint256 p2Stake = game.stake / 2; // P2 is rounded down
-            uint256 p1Stake = game.stake - p2Stake;
-            lordsToken.transfer(game.player1.addr, p1Stake);
-            lordsToken.transfer(game.player2.addr, p2Stake);
-        } else if (winner == 1) {
-            lordsToken.transfer(game.player1.addr, game.stake);
+        if (winner == 1) {
+            emit Dishonor(gameId, game.player2.addr);
         } else if (winner == 2) {
-            lordsToken.transfer(game.player2.addr, game.stake);
+            emit Dishonor(gameId, game.player1.addr);
         }
 
-        delete games[gameId];
+        finishGame(gameId, game, winner);
     }
 
     function calculateWinner(
@@ -187,6 +200,26 @@ contract Pistols {
         uint256 fee = max(MIN_STAKE, game.stake / 100);
         lordsToken.transfer(owner, fee);
         game.stake -= fee;
+
+        if (!allPlayerStats[game.player1.addr].init) {
+            allPlayerStats[game.player1.addr] = PlayerStats({
+                init: true,
+                wins: 0,
+                losses: 0,
+                draws: 0,
+                dishonors: 0
+            });
+        }
+
+        if (!allPlayerStats[game.player2.addr].init) {
+            allPlayerStats[game.player2.addr] = PlayerStats({
+                init: true,
+                wins: 0,
+                losses: 0,
+                draws: 0,
+                dishonors: 0
+            });
+        }
 
         game.state = STATE_SHOOT_STEP_COMMITMENTS;
         game.activityDeadline = block.timestamp + timeout;
@@ -235,7 +268,11 @@ contract Pistols {
         emit GameUpdated(gameId);
     }
 
-    function revealShootStep(uint256 gameId, uint256 salt, uint256 shootStep) external {
+    function revealShootStep(
+        uint256 gameId,
+        bytes32 salt,
+        uint256 shootStep
+    ) external {
         Game storage game = games[gameId];
         requireNotTimedOut(game);
 
@@ -278,6 +315,8 @@ contract Pistols {
             revert("Only players can submit shoot step reveals");
         }
 
+        game.rand |= salt;
+
         if (
             game.player1.shootStep != UNSET &&
             game.player2.shootStep != UNSET
@@ -286,6 +325,92 @@ contract Pistols {
         }
 
         game.activityDeadline = block.timestamp + timeout;
+        emit GameUpdated(gameId);
+    }
+
+    function shootout(uint256 gameId) external {
+        Game storage game = games[gameId];
+        requireNotTimedOut(game);
+
+        require(
+            game.state == STATE_SHOOT,
+            "Game is not in shoot state"
+        );
+
+        uint256 p1ShootStep = game.player1.shootStep;
+        uint256 p2ShootStep = game.player2.shootStep;
+
+        if (p1ShootStep == FULL_STEP_COUNT && p2ShootStep == FULL_STEP_COUNT) {
+            bool p2Killed = chance(1, 2, game.rand, "p1 shot");
+            bool p1Killed = chance(1, 2, game.rand, "p2 shot");
+            
+            if (p1Killed && p2Killed) {
+                finishGame(gameId, game, 0);
+                return;
+            }
+            
+            if (p1Killed) {
+                finishGame(gameId, game, 2);
+                return;
+            }
+            
+            if (p2Killed) {
+                finishGame(gameId, game, 1);
+                return;
+            }
+        } else {
+            if (
+                p1ShootStep <= p2ShootStep &&
+                chance(p1ShootStep, 10, game.rand, "p1 shot")
+            ) {
+                emit Dishonor(gameId, game.player1.addr);
+                allPlayerStats[game.player1.addr].dishonors++;
+                game.player2.injured = true;
+            }
+
+            if (
+                p2ShootStep <= p1ShootStep &&
+                chance(p2ShootStep, 10, game.rand, "p2 shot")
+            ) {
+                emit Dishonor(gameId, game.player2.addr);
+                allPlayerStats[game.player2.addr].dishonors++;
+                game.player1.injured = true;
+            }
+        }
+
+        game.state = STATE_BATTLE_CHOICE_COMMITMENTS;
+
+        game.activityDeadline = block.timestamp + timeout;
+        emit GameUpdated(gameId);
+    }
+
+    function finishGame(
+        uint256 gameId,
+        Game storage game,
+        uint256 winner
+    ) internal {
+        if (winner == 0) {
+            uint256 p2Stake = game.stake / 2; // P2 is rounded down
+            uint256 p1Stake = game.stake - p2Stake;
+
+            lordsToken.transfer(game.player1.addr, p1Stake);
+            lordsToken.transfer(game.player2.addr, p2Stake);
+
+            allPlayerStats[game.player1.addr].draws++;
+            allPlayerStats[game.player2.addr].draws++;
+        } else if (winner == 1) {
+            lordsToken.transfer(game.player1.addr, game.stake);
+
+            allPlayerStats[game.player1.addr].wins++;
+            allPlayerStats[game.player2.addr].losses++;
+        } else if (winner == 2) {
+            lordsToken.transfer(game.player2.addr, game.stake);
+
+            allPlayerStats[game.player2.addr].wins++;
+            allPlayerStats[game.player1.addr].losses++;
+        }
+
+        delete games[gameId];
         emit GameUpdated(gameId);
     }
 
@@ -305,5 +430,15 @@ contract Pistols {
 
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
         return a > b ? a : b;
+    }
+
+    function chance(
+        uint256 numerator,
+        uint256 denominator,
+        bytes32 rand,
+        string memory seed
+    ) public pure returns (bool) {
+        uint256 randHash = uint256(keccak256(abi.encodePacked(rand, seed)));
+        return randHash % denominator < numerator;
     }
 }
