3965ac4558930f45a19fe1a9920447ee644604d4
Andrew Morris <voltrevo@gmail.com>
2023-12-19 17:02:50 +1100

    revealShootStep

--- contracts/src/Pistols.sol
+++ contracts/src/Pistols.sol
@@ -42,6 +42,8 @@ contract Pistols {
     event GameUpdated(uint256 gameId);
     event AssertionFailed(string message);
 
+    uint256 constant public FULL_STEP_COUNT = 10;
+
     uint256 constant STATE_EMPTY = 0;
     uint256 constant STATE_CHALLENGE = 1;
     uint256 constant STATE_SHOOT_STEP_COMMITMENTS = 2;
@@ -228,6 +230,60 @@ contract Pistols {
             game.player2.shootStepCommitment != bytes32(UNSET)
         ) {
             game.state = STATE_SHOOT_STEP_REVEALS;
+        }
+
+        game.activityDeadline = block.timestamp + timeout;
+        emit GameUpdated(gameId);
+    }
+
+    function revealShootStep(uint256 gameId, uint256 salt, uint256 shootStep) external {
+        Game storage game = games[gameId];
+        requireNotTimedOut(game);
+
+        require(
+            game.state == STATE_SHOOT_STEP_REVEALS,
+            "Game is not in shoot step reveals state"
+        );
+        require(
+            1 <= shootStep && shootStep <= FULL_STEP_COUNT,
+            "shootStep must be between 1 and FULL_STEP_COUNT inclusive"
+        );
+
+        if (msg.sender == game.player1.addr) {
+            require(
+                game.player1.shootStep != UNSET,
+                "Player 1 has already revealed"
+            );
+
+            require(
+                keccak256(abi.encodePacked(salt, shootStep))
+                    == game.player1.shootStepCommitment,
+                "Shoot step does not match commitment"
+            );
+
+            game.player1.shootStep = shootStep;
+        } else if (msg.sender == game.player2.addr) {
+            require(
+                game.player2.shootStep != UNSET,
+                "Player 2 has already revealed"
+            );
+
+            require(
+                keccak256(abi.encodePacked(salt, shootStep))
+                    == game.player2.shootStepCommitment,
+                "Shoot step does not match commitment"
+            );
+
+            game.player2.shootStep = shootStep;
+        } else {
+            revert("Only players can submit shoot step reveals");
+        }
+
+        if (
+            game.player1.shootStep != UNSET &&
+            game.player2.shootStep != UNSET
+        ) {
+            game.state = STATE_SHOOT;
         }
 
         game.activityDeadline = block.timestamp + timeout;
