368fa6141b0fbb102e8fb0780736bb77a63d73e1
R13 <rbentos@yahoo.com>
2023-12-27 12:11:55 -0300

    Back to development / localhost

--- dojo/scripts/default_auth.sh
+++ dojo/scripts/default_auth.sh
@@ -18,9 +18,9 @@ echo "--------------------------------------------------------------------------
 COMPONENTS=("Duelist" "Challenge" "Pact" "Duel")
 
 for component in ${COMPONENTS[@]}; do
-  # sozo auth writer --world $WORLD_ADDRESS --rpc-url $RPC_URL $component $ACTIONS_ADDRESS
+  sozo auth writer --world $WORLD_ADDRESS --rpc-url $RPC_URL $component $ACTIONS_ADDRESS
   # SLOT
-  sozo auth writer --world $WORLD_ADDRESS --rpc-url https://api.cartridge.gg/x/pistols/katana $component 0x78af18db6887ee0eb2b339f95619e88b65aa47c7025ad42dc81c7be43b631ff --account-address 0x355df606ef197ceacb60212996047efbef81828f918c8490ac950802dd602b5
+  # sozo auth writer --world $WORLD_ADDRESS --rpc-url https://api.cartridge.gg/x/pistols/katana $component 0x78af18db6887ee0eb2b339f95619e88b65aa47c7025ad42dc81c7be43b631ff --account-address 0x355df606ef197ceacb60212996047efbef81828f918c8490ac950802dd602b5
 done
 
 echo "Default authorizations have been successfully set."
