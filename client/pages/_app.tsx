de83998726bb3b175270b74b405d537926533a46
R13 <rbentos@yahoo.com>
2024-01-30 14:36:07 -0300

    Imported new fonts

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'semantic-ui-css/semantic.min.css'
-import '/styles/index.scss'
+import '/styles/fonts.scss'
+import '/styles/styles.scss'
 import React from 'react'
 import { PistolsProvider } from '@/pistols/hooks/PistolsContext'
 import { SettingsProvider } from '@/pistols/hooks/SettingsContext'
