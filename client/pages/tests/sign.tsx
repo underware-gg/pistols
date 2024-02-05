cd67b86d015fd431d2a9eeb59b94b648ded8fa62
R13 <rbentos@yahoo.com>
2024-02-05 14:45:50 -0300

    sign tests

---
+++ client/pages/tests/sign.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Grid } from 'semantic-ui-react'
+import { useDojoAccount } from '@/dojo/DojoContext'
+import { createTypedMessage } from '@/pistols/utils/starknet'
+import AppDojo from '@/pistols/components/AppDojo'
+import { Account, typedData } from 'starknet'
+
+const Row = Grid.Row
+const Col = Grid.Column
+
+export default function IndexPage() {
+  return (
+    <AppDojo>
+      <Container text>
+        <Grid>
+          <ValidateMessage />
+        </Grid>
+      </Container>
+    </AppDojo>
+  );
+}
+
+function ValidateMessage() {
+  const { account } = useDojoAccount()
+
+  const [validated, setValidated] = useState(false)
+  useEffect(() => {
+    const _validate = async () => {
+      const td = await testTypedData(account)
+      setValidated(td)
+    }
+    _validate()
+  }, [])
+
+  return (
+    <Row columns={'equal'}>
+      <Col>
+        [{validated ? 'true' : 'false'}]
+      </Col>
+    </Row>
+  )
+}
+
+
+export async function testTypedData(account: Account) {
+  const typedMessage0 = createTypedMessage(['0x01111'])
+  const typedMessage1 = createTypedMessage(['0x1111'])
+  const typedMessage2 = createTypedMessage(['0x1112'])
+  const signature0 = await account.signMessage(typedMessage0)
+  const signature1 = await account.signMessage(typedMessage1)
+  const signature2 = await account.signMessage(typedMessage2)
+  console.log(`typedMessage0`, typedMessage0)
+  console.log(`typedMessage1`, typedMessage1)
+  console.log(`typedMessage2`, typedMessage2)
+  console.log(`messageHash0`, typedData.getMessageHash(typedMessage0, account.address))
+  console.log(`messageHash1`, typedData.getMessageHash(typedMessage1, account.address))
+  console.log(`messageHash2`, typedData.getMessageHash(typedMessage2, account.address))
+  console.log(`signature0`, signature0)
+  console.log(`signature1`, signature1)
+  console.log(`signature2`, signature2)
+  console.log(`verifyMessage 0 > 0 (true)`, await account.verifyMessage(typedMessage0, signature0))
+  console.log(`verifyMessage 0 > 1 (true)`, await account.verifyMessage(typedMessage0, signature1))
+  console.log(`verifyMessage 0 > 2 (false)`, await account.verifyMessage(typedMessage0, signature2))
+  console.log(`verifyMessage 1 > 0 (true)`, await account.verifyMessage(typedMessage1, signature0))
+  console.log(`verifyMessage 1 > 1 (true)`, await account.verifyMessage(typedMessage1, signature1))
+  console.log(`verifyMessage 1 > 2 (false)`, await account.verifyMessage(typedMessage1, signature2))
+  console.log(`verifyMessage 2 > 0 (false)`, await account.verifyMessage(typedMessage2, signature0))
+  console.log(`verifyMessage 2 > 1 (false)`, await account.verifyMessage(typedMessage2, signature1))
+  console.log(`verifyMessage 2 > 2 (true)`, await account.verifyMessage(typedMessage2, signature2))
+  let result: boolean = await account.verifyMessage(typedMessage1, signature1)
+  return result
+}
--- client/src/pistols/utils/starknet.tsx
+++ client/src/pistols/utils/starknet.tsx
@@ -1,6 +1,47 @@
-import { shortString, ec } from 'starknet'
+import {
+  ec,
+  shortString,
+  Account,
+  BigNumberish,
+  TypedData, typedData,
+  Signature,
+} from 'starknet'
 
 export const validateCairoString = (v: string): string => (v ? v.slice(0, 31) : '')
 export const stringToFelt = (v: string): string => (v ? shortString.encodeShortString(v) : '0x0')
 export const feltToString = (hex: string): string => (BigInt(hex) > 0n ? shortString.decodeShortString(hex) : '')
-export const pedersen = (a: bigint | string | number, b: bigint | string | number): bigint => (BigInt(ec.starkCurve.pedersen(BigInt(a), BigInt(b))))
+export const pedersen = (a: BigNumberish, b: BigNumberish): bigint => (BigInt(ec.starkCurve.pedersen(BigInt(a), BigInt(b))))
+export const poseidon = (values: BigNumberish[]): bigint => (BigInt(ec.starkCurve.poseidonHashMany(values.map(v => BigInt(v)))))
+
+// https://github.com/starknet-io/starknet.js/blob/develop/www/docs/guides/signature.md
+export const signMessages = async (account: Account, messages: BigNumberish[]): Promise<Signature> => {
+  const typedMessage = createTypedMessage(messages)
+  return account.signMessage(typedMessage)
+}
+export const verifyMessages = async (account: Account, messages: BigNumberish[], signature: Signature): Promise<boolean> => {
+  const typedMessage = createTypedMessage(messages)
+  return account.verifyMessage(typedMessage, signature)
+}
+
+export function createTypedMessage(messages: BigNumberish[]): TypedData {
+  const message = poseidon(messages).toString()
+  return {
+    domain: {
+      name: "Pistols",
+      chainId: "funDAOmental",
+      version: "0.1.0",
+    },
+    types: {
+      StarkNetDomain: [
+        { name: "name", type: "felt" },
+        { name: "chainId", type: "felt" },
+        { name: "version", type: "felt" },
+      ],
+      Message: [{ name: "message", type: "felt" }],
+    },
+    primaryType: "Message",
+    message: {
+      message,
+    },
+  }
+}
