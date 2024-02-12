62509cf0219f156a289f12ba6aaed535ebf77fbe
Rob Morris <rob@prismatik.com.au>
2024-02-13 00:21:23 +1100

    Secretive character

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@
 const nextConfig = {
   env: {
     SERVER_URL: process.env.SERVER_URL ?? '',
-    PROFILE_PIC_COUNT: '14',
+    PROFILE_PIC_COUNT: '15',
   },
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
