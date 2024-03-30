07e2a9d679bda89fda512c0dc6fac648cb199553
R13 <rbentos@yahoo.com>
2024-03-30 18:07:24 -0300

    Burner restore WIP

--- client/src/lib/wallet/useBurnerAccount.ts
+++ client/src/lib/wallet/useBurnerAccount.ts
@@ -1,12 +1,13 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
+import { useContract } from '@starknet-react/core'
 import { useDojoAccount } from '@/dojo/DojoContext'
-import { usePistolsContext } from '@/pistols/hooks/PistolsContext'
-import { bigintEquals } from '../utils/types'
+import { bigintEquals, bigintToHex } from '@/lib/utils/types'
+import account_abi from '@/lib/abi/braavos_account.json'
+import { Contract } from 'starknet'
 
 
-export const useBurnerAccount = (address) => {
-  // const { walletSig, hasSigned, accountIndex, dispatchSetSig, dispatchSetAccountIndex, connectOpener } = usePistolsContext()
-  const { list } = useDojoAccount()
+export const useBurnerAccount = (address: bigint) => {
+  const { list, count } = useDojoAccount()
 
   const [isDeployed, isImported] = useMemo(() => {
     const burner = list().reduce((a, v) => {
@@ -17,10 +18,21 @@ export const useBurnerAccount = (address) => {
       return [true, true]
     }
 
-    // TODO: verify if deployed but not imported
-
     return [false, false]
-  }, [address])
+  }, [address, count])
+
+  //
+  // TODO: verify if deployed but not imported
+  //
+  const { contract } = useContract({ abi: account_abi, address: bigintToHex(address) })
+  
+  useEffect(() => {
+    const _check_deployed = async () => {
+      const cc = await contract.deployed()
+      console.log('__check_contract', cc?.deployTransactionHash, cc)
+    }
+    if (contract) _check_deployed()
+  }, [contract])
 
   return {
     isDeployed,
