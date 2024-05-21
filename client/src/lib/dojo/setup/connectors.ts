0bb8c6fbc10289e85568f1f6b25481b582a6d6a7
R13 <rbentos@yahoo.com>
2024-05-21 18:20:39 -0300

    Update connectors.ts

--- client/src/lib/dojo/setup/connectors.ts
+++ client/src/lib/dojo/setup/connectors.ts
@@ -4,6 +4,7 @@ import { argent, braavos } from '@starknet-react/core'
 export const supportedConnetorIds = {
   ARGENT: argent().id,
   BRAAVOS: braavos().id,
+  // CONTROLLER: controller().id,
   DOJO_PREDEPLOYED: DojoPredeployedStarknetWindowObject.getId(),
   DOJO_BURNER: DojoBurnerStarknetWindowObject.getId(),
 }
