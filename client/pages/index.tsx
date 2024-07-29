4846fd45e4861937e6a2b79f3419cbce99a1a72e
James Addison <siliconjungle.io@gmail.com>
2024-07-29 11:42:31 -0700

    clean up entry page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,38 +1,33 @@
 import React from 'react'
-import Link from 'next/link'
 import App from '@/lib/ui/App'
 import Background from '@/pistols/components/Background'
 import Logo from '@/pistols/components/Logo'
-import { usePlayerId } from '@/lib/dojo/hooks/usePlayerId'
+import { Button } from 'semantic-ui-react'
 
 export default function IndexPage() {
-  const { playerId } = usePlayerId()
   return (
     <App>
       <Background className='BackgroundSplash'>
         <div className='AlignCenter'>
-
-          <Link href='https://pistols.underware.gg/gate'>
-            <Logo />
-          </Link>
-
+          <Logo />
           <div className='Spacer20' />
-
-          <Link href='https://pistols.underware.gg/gate'>
-            <h1 className='TitleCase'>Pistols at 10 Blocks</h1>
-          </Link>
-
+          <h1 className='TitleCase'>Pistols at 10 Blocks</h1>
           <hr />
-
           <h5>
             an [<a href='https://x.com/LootUnderworld'>Underworld</a>] game
           </h5>
           <h5>
             <span>by</span> [<a href='https://underware.gg'>Underware</a>]
           </h5>
-
+          <div className='Spacer20' />
+          <Button
+            as='a'
+            href='./gate'
+            target='_blank'
+          >
+            Enter Tavern
+          </Button>
           <div style={{ height: '5vh' }}>&nbsp;</div>
-
         </div>
       </Background>
     </App>
