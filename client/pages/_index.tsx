1b1ae167aeb0c5a8207eb8fb7c9021ad9bd59e0c
RM3 <roger@studioavante.com>
2024-08-14 16:28:33 -0400

    fixed build

--- client/pages/_index.tsx
+++ client/pages/_index.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react'
 import Background from '@/pistols/components/Background'
-import Gate from '@/pistols/components/Gate'
+// import Gate from '@/pistols/components/Gate'
 import App from '@/lib/ui/App'
 import { HeaderData } from '@/lib/ui/AppHeader'
 
@@ -10,8 +10,8 @@ export default function IndexPage() {
   }), [])
   return (
     <App headerData={headerData}>
-      <Background className='BackgroundGate'>
-        <Gate />
+      <Background className='BackgroundSplash'>
+        {/* <Gate /> */}
       </Background>
     </App>
   )
