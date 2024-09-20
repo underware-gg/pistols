ad914135b468f19339c6d880724b4baeba910025
RM3 <roger@studioavante.com>
2024-09-20 13:32:45 -0400

    useERC20TokenName() try origami abi

--- client/src/lib/abi/index.ts
+++ client/src/lib/abi/index.ts
@@ -2,9 +2,11 @@ import { Abi } from 'starknet'
 import katana_account from './katana_account.json'
 import argentx_account from './argentx_account.json'
 import braavos_account from './braavos_account.json'
+import erc20_origami from './erc20_origami.json'
 import erc20 from './erc20.json'
 
 export const katana_account_abi: Abi = katana_account
 export const argentx_account_abi: Abi = argentx_account
 export const braavos_account_abi: Abi = braavos_account
+export const erc20_origami_abi: Abi = erc20_origami
 export const erc20_abi: Abi = erc20
--- client/src/lib/utils/hooks/useERC20.tsx
+++ client/src/lib/utils/hooks/useERC20.tsx
@@ -1,24 +1,22 @@
-import {  useMemo } from 'react'
+import {  useEffect, useMemo, useState } from 'react'
 import {
-  useContractRead,
-  // useBalance,
+  useContract,
+  useBalance,
 } from '@starknet-react/core'
-import { _useBalance } from '@/lib/dojo/fix/starknet_react_core'
 import { bigintToHex, isPositiveBigint } from '@/lib/utils/types'
 import { BigNumberish } from 'starknet'
-import { erc20_abi } from '@/lib/abi'
+import { erc20_abi, erc20_origami_abi } from '@/lib/abi'
 import { feltToString } from '../starknet'
 
 export const useERC20Balance = (contractAddress: BigNumberish, ownerAddress: BigNumberish, fee: BigNumberish = 0n) => {
-  // const { data: balance } = useBalance({
-  const balance = _useBalance({
+  const { data: balance } = useBalance({
     token: bigintToHex(contractAddress),
     address: bigintToHex(ownerAddress),
     watch: true,
     refetchInterval: 1_000,
     enabled: (isPositiveBigint(contractAddress) && isPositiveBigint(ownerAddress)),
   })
-  // console.log(`BALANCE`, shortAddress(bigintToHex(contractAddress)), shortAddress(bigintToHex(ownerAddress)), balance)
+  // console.log(`BALANCE`, (bigintToHex(contractAddress)), (bigintToHex(ownerAddress)), balance)
 
   const noFundsForFee = useMemo(() => {
     if (!fee || !balance) return false
@@ -35,22 +33,53 @@ export const useERC20Balance = (contractAddress: BigNumberish, ownerAddress: Big
 }
 
 export const useERC20TokenName = (contractAddress: BigNumberish) => {
-  const { data: data_name, isError, isLoading, error } = useContractRead({
-    functionName: "name",
-    args: [],
+  const contractIsValid = useMemo(() => (isPositiveBigint(contractAddress)), [contractAddress])
+
+  const { contract } = useContract({
     abi: erc20_abi,
-    address: bigintToHex(contractAddress),
+    address: contractIsValid ? bigintToHex(contractAddress) : null,
   })
-  const { data: data_symbol } = useContractRead({
-    functionName: "symbol",
-    args: [],
-    abi: erc20_abi,
-    address: bigintToHex(contractAddress),
+  const { contract:contract_origami } = useContract({
+    abi: erc20_origami_abi,
+    address: contractIsValid ? bigintToHex(contractAddress) : null,
   })
-  //@ts-ignore
-  const tokenName = useMemo(() => (data_name?.name ? feltToString(data_name?.name) : null), [data_symbol])
-  //@ts-ignore
-  const tokenSymbol = useMemo(() => (data_symbol?.symbol ? feltToString(data_symbol?.symbol) : null), [data_symbol])
+
+  const [tokenName, setTokenName] = useState<string>(null)
+  const [tokenSymbol, setTokenSymbol] = useState<string>(null)
+
+  useEffect(() => {
+    if (!contractIsValid) {
+      setTokenName(null)
+      setTokenSymbol(null)
+    }
+  }, [contractIsValid])
+  
+  // try felt
+  useEffect(() => {
+    contract?.call('name').then((results) => {
+      const name = results?.name ? feltToString(results.name) : null
+      if (name)setTokenName(name)
+    })
+  }, [contract])
+  useEffect(() => {
+    contract?.call('symbol').then((results) => {
+      const symbol = results?.symbol ? feltToString(results.symbol) : null
+      if (symbol) setTokenSymbol(symbol)
+    })
+  }, [contract])
+
+  // try origami (ByteArray)
+  useEffect(() => {
+    contract_origami?.call('name').then((name) => {
+      if (name) setTokenName(name)
+    })
+  }, [contract_origami])
+  useEffect(() => {
+    contract_origami?.call('symbol').then((symbol) => {
+      if (symbol) setTokenSymbol(symbol)
+    })
+  }, [contract_origami])
+
   return {
     tokenName,
     tokenSymbol,
