1766dc38c14d03daa9229aedf4d4d3364a8cd4ad
RM3 <roger@studioavante.com>
2024-10-20 22:51:11 -0300

    EthSigner test

--- client/pages/tests/connect.tsx
+++ client/pages/tests/connect.tsx
@@ -1,19 +1,20 @@
 import React, { useMemo, useState } from 'react'
+import { EthSigner, Signature } from 'starknet'
 import { Container, Table, Button, Image } from 'semantic-ui-react'
 import { useAccount, useDisconnect, useNetwork } from '@starknet-react/core'
 import { usePistolsContext } from '@/pistols/hooks/PistolsContext'
 import { useDojoStatus } from '@/lib/dojo/DojoContext'
 import { useSelectedChain } from '@/lib/dojo/hooks/useChain'
 import { useTypedMessage } from '@/lib/utils/hooks/useTypedMessage'
+import { useAsyncMemo } from '@/lib/utils/hooks/useAsyncMemo'
 import { feltToString } from '@/lib/utils/starknet'
-import { bigintToHex, shortAddress } from '@/lib/utils/types'
 import { DojoStatus } from '@/lib/dojo/DojoStatus'
 import { ChainSwitcher } from '@/lib/dojo/ChainSwitcher'
 import { Messages, Revision, splitSignature } from '@/lib/utils/starknet_sign'
+import { bigintToHex, shortAddress } from '@/lib/utils/types'
 import { getConnectorIcon } from '@/lib/dojo/setup/connectors'
 import StarknetConnectModal from '@/lib/dojo/StarknetConnectModal'
 import App from '@/lib/ui/App'
-import { useAsyncMemo } from '@/lib/utils/hooks/useAsyncMemo'
 
 //@ts-ignore
 BigInt.prototype.toJSON = function () { return bigintToHex(this) }
@@ -34,6 +35,7 @@ export default function IndexPage() {
         <Connect />
         {/* <Sign revision={0} /> */}
         <Sign revision={1} />
+        <EthSign />
       </Container>
     </App>
   );
@@ -236,6 +238,109 @@ function Sign({
                 r:{shortAddress(bigintToHex(signaturePair[0]))}<br />
                 s:{shortAddress(bigintToHex(signaturePair[1]))}<br />
               </> : <>?</>}
+            </Cell>
+          </Row>
+
+          <Row columns={'equal'} verticalAlign='top'>
+            <Cell>
+              Verified
+            </Cell>
+            <Cell className='Code'>
+              {isVerified === true ? 'true' : isVerified === false ? 'false' : 'unknown'}
+            </Cell>
+          </Row>
+
+        </Body>
+      </Table>
+    </>
+  )
+}
+
+
+function EthSign({
+}: {
+}) {
+  const { account, isConnected } = useAccount()
+  const { chain } = useNetwork()
+
+  const address = '0xe29882a1fcba1e7e10cad46212257fea5c752a4f9b1b1ec683c503a2cf5c8a'
+  const privateKey = '0x14d6672dcb4b77ca36a887e9a11cd9d637d5012468175829e9c6e770c61642'
+  const isVerified = undefined
+
+  const messages: Messages = useMemo(() => ({
+    game: 'PISTOLS_AT_10_BLOCKS',
+    purpose: `ETH_SIGNER_TEST`,
+  }), [])
+  const { typedMessage, messageHash, typeHash } = useTypedMessage({
+    account,
+    revision: 1,
+    chainId: chain.id,
+    messages,
+  })
+
+  const [isSigning, setIsSigning] = useState<boolean>(false)
+  const [signature, setSignature] = useState<Signature | string[]>([])
+
+  const _sign = async () => {
+    console.log(`SIGN message:`, typedMessage)
+    setIsSigning(true)
+    setSignature([])
+    const myEthSigner = new EthSigner(privateKey)
+    // const pubKey = await myEthSigner.getPubKey()
+    const sig = await myEthSigner.signMessage(typedMessage, address);
+    setSignature(sig)
+    setIsSigning(false)
+  }
+
+  return (
+    <>
+      <Button disabled={!isConnected || isSigning} onClick={() => _sign()}>ETH Sign</Button>
+      <Table celled striped size='small' color={isVerified == true ? 'green' : isVerified === false ? 'red' : 'orange'}>
+        <Body>
+          <Row columns={'equal'} verticalAlign='top'>
+            <Cell>
+              Message
+            </Cell>
+            <Cell className='Code'>
+              {Object.keys(messages).map((k, i) => <React.Fragment key={k}>{k}:{(messages[k] as string)}<br /></React.Fragment>)}
+            </Cell>
+          </Row>
+
+          <Row columns={'equal'} verticalAlign='top'>
+            <Cell className='Code'>
+              Typed Data
+            </Cell>
+            <Cell className='Code'>
+              {JSON.stringify(typedMessage)}
+            </Cell>
+          </Row>
+
+          <Row columns={'equal'} verticalAlign='top'>
+            <Cell>
+              Type Hash
+            </Cell>
+            <Cell className='Code'>
+              {bigintToHex(typeHash)}
+            </Cell>
+          </Row>
+
+          <Row columns={'equal'} verticalAlign='top'>
+            <Cell>
+              Message Hash
+            </Cell>
+            <Cell className='Code'>
+              {bigintToHex(messageHash)}
+            </Cell>
+          </Row>
+
+          <Row columns={'equal'} verticalAlign='top'>
+            <Cell>
+              Signature
+            </Cell>
+            <Cell className='Code'>
+              {/* {signature ?? <>?</>} */}
+              {/* ({signature.length})  */}
+              {JSON.stringify(signature, null, ' ')}
             </Cell>
           </Row>
 
