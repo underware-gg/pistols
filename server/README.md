7a006b24c13e9ff486f8233b826189b6700ff19f
RM3 <roger@studioavante.com>
2025-03-07 12:57:16 -0300

    moved server to assets-server repo

--- server/README.md
+++
@@ -1,1 +0,0 @@
-# Underware assets server
--- server/src/app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-// next.js app routerAPI routes
-// https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
-export async function GET(request: Request) {
-  return new Response('Hello, Underworld!', {
-    status: 200,
-    // headers: {},
-  })
-}
