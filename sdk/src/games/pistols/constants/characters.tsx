b6bd82445f1320f45febc39cbb21648255d5b5a1
RM3 <roger@studioavante.com>
2025-07-24 15:45:29 -0300

    character constants helpers

--- sdk/src/games/pistols/constants/characters.tsx
+++ sdk/src/games/pistols/constants/characters.tsx
@@ -1,3 +1,5 @@
+import { BigNumberish } from "starknet";
+import { bigintToAddress } from "src/exports/utils";
 
 export const CHARACTER_NAMES: Record<string, string> = {
   '0x04D92577856263bDe8E7601Ee189b6dbe52aCb879462489B92c0789f6c157E6c': '[Pistols Deployer]', // contracts deployer
@@ -8,4 +10,11 @@ export const CHARACTER_NAMES: Record<string, string> = {
 export const CHARACTER_AVATARS: Record<string, string> = {
   '0x0569d6f6080a3aB8678738De7Da68097796b11ECE78b21fD7FAe2Fd7505AB0Ba': '/profiles/characters/01.jpg',
   '0x05d9c9168e6bf86f250a813d38ad5a95c67cb3ca42ab93411a184c9c731fa69a': '/profiles/characters/05.jpg',
+}
+
+export function getCharacterAccountName(address: BigNumberish): string | undefined {
+  return CHARACTER_NAMES[bigintToAddress(address)];
+}
+export function getCharacterAccountAvatar(address: BigNumberish): string | undefined {
+  return CHARACTER_AVATARS[bigintToAddress(address)];
 }
