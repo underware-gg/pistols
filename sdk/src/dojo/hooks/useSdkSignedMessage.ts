3a76d8e3deef073d2aff87a568b6df86129f526a
RM3 <roger@studioavante.com>
2025-08-01 15:03:35 -0300

    auto online status refactor

--- sdk/src/dojo/hooks/useSdkSignedMessage.ts
+++ sdk/src/dojo/hooks/useSdkSignedMessage.ts
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react'
-import { Account, TypedData, stark } from 'starknet'
+import { Account, TypedData } from 'starknet'
 import { useDojoSetup } from 'src/dojo/contexts/DojoContext'
 import { useConnectedController } from 'src/dojo/hooks/useController'
 import { debug } from 'src/games/pistols/misc/debug'
@@ -8,59 +8,42 @@ export const useSdkPublishTypedData = (
   account: Account,
   typedData: TypedData,
 ) => {
-  const { sdk } = useDojoSetup()
-  const { isControllerConnected } = useConnectedController()
-  const [isPublishing, setIsPublishing] = useState<boolean>()
+  const { sdk } = useDojoSetup();
+  const { isControllerConnected } = useConnectedController();
+  const [isPublishing, setIsPublishing] = useState<boolean>();
+  const [isSuccess, setIsSuccess] = useState<boolean>();
 
   const publish = useCallback(async () => {
     if (sdk && typedData && account) {
       if (!isControllerConnected) {
-        console.warn('useSdkPublishSignedMessage() needs Cartridge Controller!')
-        return
+        console.warn('useSdkPublishSignedMessage() needs Cartridge Controller!');
+        return;
       }
       if (isPublishing) {
-        console.warn('useSdkPublishSignedMessage() still publishing...')
-        return
+        console.warn('useSdkPublishSignedMessage() still publishing...');
+        return;
       }
 
-      setIsPublishing(true)
+      setIsPublishing(true);
+      setIsSuccess(undefined);
 
       try {
-        debug.log(`SIGNED_MESSAGE: publish..`, typedData);
-        sdk.sendMessage(typedData, account);
+        debug.log(`SIGNED_MESSAGE: publish...`, typedData);
+        await sdk.sendMessage(typedData, account);
+        setIsSuccess(true);
       } catch (error) {
+        setIsSuccess(false);
         console.error("useSdkPublishSignedMessage() failed to publish message:", error, typedData);
       }
-
-      // try {
-      //   // debug.log('SIGNED_MESSAGE: sign...', serialize(typedData), typedData)
-      //   let signature = await account.signMessage(typedData);
-      //   // debug.log('SIGNED_MESSAGE: signature:', signature)
-      //   if (!Array.isArray(signature)) {
-      //     signature = stark.formatSignature(signature)
-      //   }
-
-      //   try {
-      //     debug.log(`SIGNED_MESSAGE: publish... len:${signature.length}`)//, signature)
-      //     await sdk.client.publishMessage(
-      //       JSON.stringify(typedData),
-      //       signature as string[],
-      //     );
-      //     debug.log('SIGNED_MESSAGE: published!')
-      //   } catch (error) {
-      //     console.error("useSdkPublishSignedMessage() failed to publish message:", error, typedData, signature);
-      //   }
-      // } catch (error) {
-      //   console.error("useSdkPublishSignedMessage() failed to sign message:", error, typedData);
-      // }
-
+      setIsPublishing(false);
       // debug.log('SIGNED: done!')
-      setIsPublishing(false)
     }
   }, [sdk, typedData, account, isControllerConnected])
 
   return {
     isPublishing,
+    isSuccess,
+    isError: (isSuccess === undefined ? undefined : isSuccess === false),
     publish,
   }
 }
