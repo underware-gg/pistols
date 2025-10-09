44a68c787b5a9f623021a803363a659a9a1164ca
RM3 <roger@studioavante.com>
2025-10-09 14:02:21 -0300

    added Cartridge Airdropper contract as admin

--- sdk/src/games/pistols/constants/characters.tsx
+++ sdk/src/games/pistols/constants/characters.tsx
@@ -5,6 +5,7 @@ export const CHARACTER_NAMES: Record<string, string> = {
   '0x04D92577856263bDe8E7601Ee189b6dbe52aCb879462489B92c0789f6c157E6c': '[Pistols Deployer]', // contracts deployer
   '0x0569d6f6080a3aB8678738De7Da68097796b11ECE78b21fD7FAe2Fd7505AB0Ba': 'The Bartender', // auto reveal account
   '0x05d9c9168e6bf86f250a813d38ad5a95c67cb3ca42ab93411a184c9c731fa69a': 'Imp Master', // bot_player contract
+  '0x002346e4ccfc96bee8eb5391344f5f5ab7bf17d36c566888fe39e8816d0a50e7': '[Cartridge Airdropper]', // merkle drop contract
 }
 
 export const CHARACTER_AVATARS: Record<string, string> = {
