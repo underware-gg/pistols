f9cab62b4697d3442d09a902a35118434ba06300
RM3 <roger@studioavante.com>
2025-12-18 10:50:17 -0300

    migrate mainnet

--- sdk/src/games/pistols/config/config.ts
+++ sdk/src/games/pistols/config/config.ts
@@ -4,12 +4,13 @@ import _pistols_manifest_dev from './manifests/manifest_dev.json'
 import pistols_manifest_slot from './manifests/manifest_slot.json'
 import pistols_manifest_staging from './manifests/manifest_staging.json'
 import _pistols_manifest_sepolia from './manifests/manifest_sepolia.json'
-import pistols_manifest_mainnet from './manifests/manifest_mainnet.json'
+import _pistols_manifest_mainnet from './manifests/manifest_mainnet.json'
 import { convert_dojo_manifest_1_8_to_1_7 } from 'src/fix/manifest_converter'
 import { bigintToAddress } from 'src/utils/misc/types'
 
 const pistols_manifest_dev = convert_dojo_manifest_1_8_to_1_7(_pistols_manifest_dev);
 const pistols_manifest_sepolia = convert_dojo_manifest_1_8_to_1_7(_pistols_manifest_sepolia);
+const pistols_manifest_mainnet = convert_dojo_manifest_1_8_to_1_7(_pistols_manifest_mainnet);
 
 export const NAMESPACE = 'pistols'
 
